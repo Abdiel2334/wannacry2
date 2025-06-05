e37114108ded020096f43109a272bb8fdf284feb
Abdiel2334 <ruizivan24@Gmail.com>
2025-06-04 20:09:35 -0700

    Add files via upload

---
+++ wannacry.bat
@@ -0,0 +1,15 @@
+ho off
+color a
+taskkill /f /im "explorer.exeecho Hello your device is infected with wannaTry
+echo Hit any key to proceed
+pause
+:3
+del speech.Vbs
+echo set speech = Wscript.CreateObject("SAPI.spVoice") >> "speech.Vbs"
+set "text=Lol just kidding it was a prank"
+echo speech.speak "%text%" >> "speech.vbs"
+start speech.vbs
+:4
+echo lol just kidding it was a prank
+start explorer.exe
+pause
